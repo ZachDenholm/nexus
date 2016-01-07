db15b779120935a6e110c7ae655612f9babd81d4
rcwd <rob@openrock.co>
2016-01-07 16:41:17 +0000

    Working out categories

--- archive-dataset.php
+++ archive-dataset.php
@@ -1,12 +1,23 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<?php get_template_part('templates/page', 'header'); 
 
-<?php if (!have_posts()) : ?>
+$contentArgs = array(
+    'post_type' => array( 'datasets'),
+    'cat' => 14,
+    'posts_per_page' => 10,
+    'orderby' => 'modified',
+    'order' => 'DESC',
+    'paged' => ( get_query_var('paged') ) ? get_query_var('paged') : 1,
+    );
+$wp_query = new WP_Query( $contentArgs );
+?>
+
+<?php if (!$wp_query->have_posts()) : ?>
   <div class="alert alert-warning">
     <?php _e('Sorry, no results were found...', 'sage'); ?>
   </div>
   <?php get_search_form(); ?>
 <?php endif; ?>
-<?php while (have_posts()) : the_post(); ?>
+<?php while ($wp_query->have_posts()) : the_post(); ?>
   <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
 <?php endwhile; ?>
 
--- templates/header.php
+++ templates/header.php
@@ -1,19 +1,19 @@
 <header class="banner">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+    
     <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-      <?php if ( is_user_logged_in() ): ?>
-        <a href="<?php echo wp_logout_url( $redirect ); ?>" class="auth logout">Logout</a>
-      <?php else: ?>
-        <a href="" class="auth login">Login</a>
-        <a href="/register/" class="auth register">Register</a>
-      <?php endif; ?>
-      
+      <ul id="menu-main" class="nav">
+        <li class="menu-item menu-home"><a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></li>
+        <li class="active menu-item menu-datasets"><a href="/datasets/">Approved Datasets</a></li>
+        <li class="active menu-item menu-pending-datasets"><a href="/category/status/pending/">Pending Datasets</a></li>
+        <li class="menu-item menu-submit-a-dataset"><a href="https://nexus.data.je/dataset-submission/">Submit a Dataset</a></li>
+        <?php if ( is_user_logged_in() ): ?>
+          <li class="menu-item menu-your-profile"><a href="<?php echo wp_logout_url( $redirect ); ?>" class="auth logout">Logout</a>
+        <?php else: ?>
+          <li class="menu-item menu-login"><a href="" class="auth login">Login</a></li>
+          <li class="menu-item menu-register"><a href="/register/" class="auth register">Register</a></li>
+        <?php endif; ?>
+      </ul>
     </nav>
   </div>
 </header>
