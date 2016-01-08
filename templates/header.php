ed6155c9c709b5edc6902a17393e41470af179db
rcwd <rob@openrock.co>
2016-01-08 09:03:13 +0000

    Updated login route

--- templates/header.php
+++ templates/header.php
@@ -10,7 +10,7 @@
         <?php if ( is_user_logged_in() ): ?>
           <li class="menu-item menu-your-profile"><a href="<?php echo wp_logout_url( $redirect ); ?>" class="auth logout">Logout</a>
         <?php else: ?>
-          <li class="menu-item menu-login"><a href="" class="auth login">Login</a></li>
+          <li class="menu-item menu-login"><a href="/login/" class="auth login">Login</a></li>
           <li class="menu-item menu-register"><a href="/register/" class="auth register">Register</a></li>
         <?php endif; ?>
       </ul>
