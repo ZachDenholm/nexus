6fdbeddb67641f18f67689b9e8ff6dd514ae766d
rcwd <rob@openrock.co>
2016-01-08 09:23:42 +0000

    Added footer menu

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,10 @@
 <footer class="content-info">
   <div class="container">
+    <?php
+    if (has_nav_menu('footer_navigation')) :
+      wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'nav']);
+    endif;
+    ?>
     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
 </footer>
