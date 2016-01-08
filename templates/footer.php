c832e00c6bb63ba763470ec28ca277d11896c94e
rcwd <rob@openrock.co>
2016-01-08 09:25:21 +0000

    Added footer menu

--- templates/footer.php
+++ templates/footer.php
@@ -1,10 +1,17 @@
 <footer class="content-info">
   <div class="container">
-    <?php
-    if (has_nav_menu('footer_navigation')) :
-      wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'nav']);
-    endif;
-    ?>
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+    <div class="copyright">
+        <a href="/license">&copy; Copyright <?php echo date('Y'); >?> Date.je. Some Rights Reserved</a>
+    </div>
+    <div class="menu">
+        <?php
+        if (has_nav_menu('footer_navigation')) :
+          wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'nav']);
+        endif;
+        ?>
+    </div>
+    <div class="widgets">
+        <?php dynamic_sidebar('sidebar-footer'); ?>
+    </div>
   </div>
 </footer>
