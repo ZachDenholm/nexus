d212618cb0af91ac5d74b74775b17a658c72bb90
rcwd <rob@openrock.co>
2016-01-08 09:20:43 +0000

    Readded menu

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,12 @@
 <header class="banner">
   <div class="container">
-    
     <nav class="nav-primary">
       <ul id="menu-main" class="nav">
-        <li class="menu-item menu-home"><a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></li>
-        <li class="active menu-item menu-datasets"><a href="/datasets/">Approved Datasets</a></li>
-        <li class="active menu-item menu-pending-datasets"><a href="/category/status/pending/">Pending Datasets</a></li>
-        <li class="menu-item menu-submit-a-dataset"><a href="https://nexus.data.je/dataset-submission/">Submit a Dataset</a></li>
+        <?php
+        if (has_nav_menu('primary_navigation')) :
+          wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'container' => false]);
+        endif;
+        ?>
         <?php if ( is_user_logged_in() ): ?>
           <li class="menu-item menu-your-profile"><a href="<?php echo wp_logout_url( $redirect ); ?>" class="auth logout">Logout</a>
         <?php else: ?>
