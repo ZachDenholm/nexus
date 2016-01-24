8f02a3fc594153d33505e7dcf2fc7d293ec065a5
rcwd <rob@rcwd.me>
2016-01-24 18:25:59 +0000

    Functions

--- functions.php
+++ functions.php
@@ -27,6 +27,33 @@ foreach ($sage_includes as $file) {
 }
 unset($file, $filepath);
 
+/**
+ * Redirect user after successful login.
+ *
+ * @param string $redirect_to URL to redirect to.
+ * @param string $request URL the user is coming from.
+ * @param object $user Logged user's data.
+ * @return string
+ */
+function my_login_redirect( $redirect_to, $request, $user ) {
+  //is there a user to check?
+  global $user;
+  if ( isset( $user->roles ) && is_array( $user->roles ) ) {
+    //check for admins
+    if ( in_array( 'administrator', $user->roles ) ) {
+      // redirect them to the default place
+      return $redirect_to;
+    } else {
+      return home_url();
+    }
+  } else {
+    return $redirect_to;
+  }
+}
+
+add_filter( 'login_redirect', 'my_login_redirect', 10, 3 );
+
+// Add taxonomies to custom types
 function add_custom_types_to_tax( $query ) {
     if( is_category() || is_tag() && empty( $query->query_vars['suppress_filters'] ) ) {
         // Get all your post types
