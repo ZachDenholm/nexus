2d58a7f707e5859e51d035738d2b1a953755ed9a
ZachDenholm <Zach@zacharys-MacBook-Pro.local>
2016-02-21 21:39:20 +0000

    adding bootstrap mobile menu
    replacing the main nav with the bootstrap navbar

--- templates/header.php
+++ templates/header.php
@@ -1,20 +1,41 @@
 <header class="banner">
   <div class="container">
     <h1>Data.je - The Jersey Open Data Project</h1>
-    <nav class="nav-primary">
-      <ul id="menu-main" class="nav">
+<nav class="navbar nav-primary" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+              <div id="navbar" class="navbar-collapse collapse">
+
         <?php
-        if (has_nav_menu('primary_navigation')) :
-          wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'container' => false]);
-        endif;
+            wp_nav_menu( array(
+                'menu'              => 'primary',
+                'theme_location'    => 'primary',
+                'depth'             => 2,
+                'container'         => 'div',
+                'container_class'   => 'collapse navbar-collapse',
+                'container_id'      => 'navbar',
+                'menu_class'        => 'nav navbar-nav',
+                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+                'walker'            => new wp_bootstrap_navwalker())
+            );
         ?>
+
+                    <ul class="nav navbar-nav ">
         <?php if ( is_user_logged_in() ): ?>
           <li class="menu-item menu-your-profile"><a href="<?php echo wp_logout_url( $redirect ); ?>" class="auth logout">Logout</a>
         <?php else: ?>
           <li class="menu-item menu-login"><a href="/login/" class="auth login">Login</a></li>
           <li class="menu-item menu-register"><a href="/register/" class="auth register">Register</a></li>
         <?php endif; ?>
-      </ul>
-    </nav>
+            </ul>
+        </div>
+</nav>
   </div>
 </header>
